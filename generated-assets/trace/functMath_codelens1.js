25c12384b2ebc36faeb65daf3a4671f67e380a0d
Barbara Ericson <43884467+barbarer@users.noreply.github.com>
2026-06-19 15:46:07 -0400

    Updates and select question test

--- generated-assets/trace/functMath_codelens1.js
+++ generated-assets/trace/functMath_codelens1.js
@@ -1,4 +1,12 @@
+
 if (allTraceData === undefined) {
- var allTraceData = {};
- }
- allTraceData["rs-functMath_codelens1"] = {"code": "import math\nnum = 2.0\nresult = math.sqrt(4) / num\nprint(result)\n\n", "trace": [{"line": 1, "event": "step_line", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": ""}, {"line": 2, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1]}, "ordered_globals": ["math"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 3, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"]}, "ordered_globals": ["math", "num"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 4, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"], "result": ["SPECIAL_FLOAT", "1.0"]}, "ordered_globals": ["math", "num", "result"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 4, "event": "return", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"], "result": ["SPECIAL_FLOAT", "1.0"]}, "ordered_globals": ["math", "num", "result"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": "1.0\n"}]}
+    var allTraceData = {};
+}
+(function() { // IIFE to avoid variable collision
+    let codelensID = "rs-functMath_codelens1";  //fallback
+    let partnerCodelens = document.currentScript.parentElement.querySelector(".pytutorVisualizer");
+    if (partnerCodelens) {
+        codelensID = partnerCodelens.id;
+    }
+    allTraceData[codelensID] = {"code": "import math\nnum = 2.0\nresult = math.sqrt(4) / num\nprint(result)\n", "trace": [{"line": 1, "event": "step_line", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": ""}, {"line": 2, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1]}, "ordered_globals": ["math"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 3, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"]}, "ordered_globals": ["math", "num"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 4, "event": "step_line", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"], "result": ["SPECIAL_FLOAT", "1.0"]}, "ordered_globals": ["math", "num", "result"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": ""}, {"line": 4, "event": "return", "func_name": "<module>", "globals": {"math": ["REF", 1], "num": ["SPECIAL_FLOAT", "2.0"], "result": ["SPECIAL_FLOAT", "1.0"]}, "ordered_globals": ["math", "num", "result"], "stack_to_render": [], "heap": {"1": ["INSTANCE", "module"]}, "stdout": "1.0\n"}], "startingInstruction": 0};
+})();
